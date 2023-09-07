861a9a8dbadc5d3772a182eb5767dc2d3cee80b1
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2023-09-07 17:51:58 +0300

    2

--- src/main/java/sia/tacocloud/HomeController.java
+++ src/main/java/sia/tacocloud/HomeController.java
@@ -1,2 +1,14 @@
-package sia.tacocloud;public class HomeController {
+package sia.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
